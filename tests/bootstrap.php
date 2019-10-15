486adfe43e5af2c4a6c03ebda3938415466fa535
s00d <Virus191288@gmail.com>
2019-10-15 14:07:52 +0500

    fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,20 +1,20 @@
 <?php
-    if (!function_exists('dd')) {
-        function dd()
-        {
-            $args = func_get_args();
-            call_user_func_array('dump', $args);
-            die();
-        }
-    }
-
-    if (!function_exists('d')) {
-        function d()
-        {
-            $args = func_get_args();
-            call_user_func_array('dump', $args);
-        }
-    }
+//    if (!function_exists('dd')) {
+//        function dd()
+//        {
+//            $args = func_get_args();
+//            call_user_func_array('dump', $args);
+//            die();
+//        }
+//    }
+//
+//    if (!function_exists('d')) {
+//        function d()
+//        {
+//            $args = func_get_args();
+//            call_user_func_array('dump', $args);
+//        }
+//    }
 
     $loader = require __DIR__ . '/../vendor/autoload.php';
 
